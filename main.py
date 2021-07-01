1797417f7680d3935e956d3471b38261b8fb0189
DALALGHAMDI <dal.alghamdi@hotmail.com>
2021-06-30 18:55:29 -0700

    update email and password

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ mail= Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'dal.alghamdi92@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Dalia280678'
+app.config['MAIL_USERNAME'] = 'biobankingportal@gmail.com'
+app.config['MAIL_PASSWORD'] = 'NGBO121212!'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
