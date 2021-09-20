9179064e0f10531f27c9f3ac1bda15f0c4b791e7
Dalia Alghamdi <dal.alghamdi@hotmail.com>
2021-09-20 11:37:57 -0700

    added search filters
    '

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello")
+
+def Display(msg):
+    print(msg)
+
+Display("Testing")
+
+for i in range(10):
+    Display("Time Pass")
--- main.py
+++ main.py
@@ -23,7 +23,9 @@ prefixquery = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
                 PREFIX obo: <http://purl.obolibrary.org/obo/>
-                PREFIX ngbo: <http://purl.obolibrary.org/obo/ngbo.owl#> """
+                PREFIX ngbo: <http://purl.obolibrary.org/obo/ngbo.owl/> """
+
+
 url = 'http://localhost:3030/NGBO/sparql'
 
 sparql = SPARQLWrapper("http://localhost:7200/repositories/NGBO")
@@ -58,9 +60,13 @@ def Index():
     #results = r.json()
     return render_template("index.html", data=results)
 
+@app.route('/RequestPage')
+def RequestPage():
+    return render_template("requestpage.html")
 
-@app.route('/speciman')
-def Speciman():
+
+@app.route('/specimen')
+def Specimen():
     sparql.setQuery(prefixquery + """ 
                 SELECT ?entity ?label
                 WHERE {
@@ -72,20 +78,20 @@ def Speciman():
     #r = requests.get(url, params={'format': 'json', 'query': query})
     #results = r.json()
     #print(results)
-    return render_template("speciman.html", data=results)
+    return render_template("specimen.html", data=results)
 
 
-@app.route('/SearchBySpeciman')
-def SearchBySpeciman():
-    select = str(request.args.get('speciman_name'))
-    speciman_code = select.rsplit('/', 1)[-1]
-    print("===> Speciman Code:", speciman_code)
+@app.route('/SearchBySpecimen')
+def SearchBySpecimen():
+    select = str(request.args.get('specimen_name'))
+    specimen_code = select.rsplit('/', 1)[-1]
+    print("===1> Specimen Code:", specimen_code)
     user = session['user']
     if user == 'GUEST':
         sparql.setQuery(prefixquery + """
                             SELECT (COUNT(?label) AS ?total) 
                             WHERE {
-                                ?entity a <http://purl.obolibrary.org/obo/""" + str(speciman_code) + """> .
+                                ?entity a <http://purl.obolibrary.org/obo/""" + str(specimen_code) + """> .
                                 ?entity rdfs:label ?label .
                             } """)
         sparql.setReturnFormat(JSON)
@@ -94,13 +100,13 @@ def SearchBySpeciman():
         # r = requests.get(url, params={'format': 'json', 'query': query})
         # results = r.json()
         print(results)
-        return render_template("countspecimanbysearch.html", data=results)
+        return render_template("countspecimenbysearch.html", data=results)
 
     else:
         sparql.setQuery(prefixquery + """
                                 SELECT ?entity ?label
                                 WHERE {
-                                    ?entity a <http://purl.obolibrary.org/obo/""" + str(speciman_code) + """> .
+                                    ?entity a <http://purl.obolibrary.org/obo/""" + str(specimen_code) + """> .
                                     ?entity rdfs:label ?label .
                                 } """)
         sparql.setReturnFormat(JSON)
@@ -108,15 +114,15 @@ def SearchBySpeciman():
 
         # r = requests.get(url, params={'format': 'json', 'query': query})
         # results = r.json()
-        print(results)
-        return render_template("specimanbysearch.html", data=results)
+        #print(results)
+        return render_template("specimenbysearch.html", data=results)
 
 
 @app.route('/checkguest')
 def checkguest():
     session['user'] = 'GUEST'
     print(session['user'])
-    return redirect('/speciman')
+    return redirect('/specimen')
 
 # SELECT ?predicate ?label ?object
 # WHERE {
@@ -150,10 +156,9 @@ def Auth():
                 ?x ngbo:userId ?uid .
                 ?x ngbo:userName ?uname .
                 ?x ngbo:userPassword ?upwd .
-                ?x ngbo:userEmailId ?email
+                ?x ngbo:userEmailId ?email .
                 Filter(?uname = '""" + uname + """' && ?upwd = '""" + upass + """')
             }""")
-
         #r = requests.get(url, params={'format': 'json', 'query': query})
         #results = r.json()
         sparql.setReturnFormat(JSON)
@@ -163,30 +168,110 @@ def Auth():
 
         if int(count) > 0:
             session['user'] = uname
-            return redirect('/speciman')
+            return redirect('/specimen')
         else:
             message = "Invalid username and password"
             return render_template("login.html", message = message)
 
 
-@app.route('/MoreInfoOfSpeciman')
-def MoreInfoOfSpeciman():
-    select = str(request.args.get('speciman_name'))
-    speciman_code = select.rsplit('/', 1)[-1]
-    print("===> Speciman Code:", speciman_code)
+@app.route('/DetailsOfSpecimen')
+def DetailsOfSpecimen():
+    sparql.setQuery(prefixquery + """ 
+            SELECT ?bloodspecimenlabel ?biobanklabel ?libraryPreprationlabel ?LibraryPreprationProtocollabel
+            ?LibraryPreprationKitlabel ?Sequencingdevicelabel ?availableSequencingDatalabel ?FileFormatlabel
+            WHERE{
+            
+            ?bloodspecimen a obo:OBI_0000655 .
+            ?bloodspecimen rdfs:label ?bloodspecimenlabel .
+            
+            ?biobank a obo:OMIABIS_0000000 .
+            ?biobank rdfs:label ?biobanklabel .
+                
+            ?libraryPrepration a obo:OBI_0000711 .   
+            ?libraryPrepration rdfs:label ?libraryPreprationlabel . 
+                
+            ?LibraryPreprationProtocol a ngbo:NGBO_6000276 .   
+            ?LibraryPreprationProtocol rdfs:label ?LibraryPreprationProtocollabel .
+                
+            ?LibraryPreprationKit a ngbo:NGBO_6000255 .
+            ?LibraryPreprationKit rdfs:label ?LibraryPreprationKitlabel .
+                
+            ?Sequencingdevice a obo:OBI_0400103 .
+            ?Sequencingdevice rdfs:label ?Sequencingdevicelabel .
+                
+            ?availableSequencingData a obo:OBI_0001573 .
+            ?availableSequencingData rdfs:label ?availableSequencingDatalabel .
+                
+            ?FileFormat a ngbo:NGBO_6000003 .
+            ?FileFormat rdfs:label ?FileFormatlabel .
+                
+            ?bloodspecimen obo:BFO_0000050 ?biobank .
+            ?libraryPrepration obo:OBI_0000293 ?bloodspecimen .
+            ?libraryPrepration obo:STATO_0000102 ?LibraryPreprationProtocol .
+            ?libraryPrepration obo:OBI_0000293 ?LibraryPreprationKit .
+            ?availableSequencingData obo:IAO_0000136 ?bloodspecimen .
+            ?FileFormat obo:IAO_0000136 ?availableSequencingData .
+            
+            }
+            """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    # r = requests.get(url, params={'format': 'json', 'query': query})
+    # results = r.json()
+    print("Detail Relationship===>",results)
+    return render_template("details_of_specimen.html", data=results)
+
+
+@app.route('/MoreInfoOfSpecimen')
+def MoreInfoOfSpecimen():
+    print(request.args.get('chkbloodspecimen'))
+    select = str(request.args.get('chkbloodspecimen'))
+    specimen_code = select[-4:]
+    print("===> Specimen Code:", specimen_code)
     sparql.setQuery(prefixquery + """
-                    SELECT ?predicate ?label ?object
-                    WHERE {
-                       ?predicate ?object <http://purl.obolibrary.org/obo/""" + str(speciman_code) + """> .
-                       ?predicate rdfs:label ?label .
-                    } """)
+                     SELECT ?bloodspecimenlabel ?biobanklabel ?libraryPreprationlabel ?LibraryPreprationProtocollabel
+            ?LibraryPreprationKitlabel ?Sequencingdevicelabel ?availableSequencingDatalabel ?FileFormatlabel
+            WHERE{
+            
+            ?bloodspecimen a obo:OBI_0000655 .
+            ?bloodspecimen rdfs:label ?bloodspecimenlabel .
+            
+            ?biobank a obo:OMIABIS_0000000 .
+            ?biobank rdfs:label ?biobanklabel .
+                
+            ?libraryPrepration a obo:OBI_0000711 .   
+            ?libraryPrepration rdfs:label ?libraryPreprationlabel . 
+                
+            ?LibraryPreprationProtocol a ngbo:NGBO_6000276 .   
+            ?LibraryPreprationProtocol rdfs:label ?LibraryPreprationProtocollabel .
+                
+            ?LibraryPreprationKit a ngbo:NGBO_6000255 .
+            ?LibraryPreprationKit rdfs:label ?LibraryPreprationKitlabel .
+                
+            ?Sequencingdevice a obo:OBI_0400103 .
+            ?Sequencingdevice rdfs:label ?Sequencingdevicelabel .
+                
+            ?availableSequencingData a obo:OBI_0001573 .
+            ?availableSequencingData rdfs:label ?availableSequencingDatalabel .
+                
+            ?FileFormat a ngbo:NGBO_6000003 .
+            ?FileFormat rdfs:label ?FileFormatlabel .
+                
+            ?bloodspecimen obo:BFO_0000050 ?biobank .
+            ?libraryPrepration obo:OBI_0000293 ?bloodspecimen .
+            ?libraryPrepration obo:STATO_0000102 ?LibraryPreprationProtocol .
+            ?libraryPrepration obo:OBI_0000293 ?LibraryPreprationKit .
+            ?availableSequencingData obo:IAO_0000136 ?bloodspecimen .
+            ?FileFormat obo:IAO_0000136 ?availableSequencingData .
+            FILTER regex(?bloodspecimenlabel, '"""+ specimen_code +"""', "i")
+            } """)
 
     #r = requests.get(url, params={'format': 'json', 'query': query})
     #results = r.json()
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     print(results)
-    return render_template("more_info_of_speciman.html", data=results)
+    return render_template("more_info_of_specimen.html", data=results)
 
 
 @app.route("/EmailSent", methods=['POST', 'GET'])
